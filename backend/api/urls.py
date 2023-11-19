af0338e94bca010c48a397bc6667527ac41665af
Gaurav-jo1 <gj451026@gmail.com>
2023-11-19 16:19:19 +0530

    Class based API added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.languages_list, name="get_or_add_languages"),
+    path('class_add/', views.languages_class_list.as_view(), name="class_get_or_add_languages"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,15 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from rest_framework.parsers import JSONParser
 
 from .models import ProgrammingLanguages
 from .serializers import ProgrammingLanguages_Serializer
 
+from rest_framework.views import APIView
+
 
 # Create your views here.
+# 1. Function Based View
 @api_view(["GET", "POST"])
 @permission_classes([AllowAny])
 def languages_list(request):
@@ -27,7 +29,7 @@ def languages_list(request):
         return Response(
             data={
                 "message": "No data available.",
-                "languages": [],
+                "languages": None,
             },
             status=status.HTTP_204_NO_CONTENT,
         )
@@ -45,6 +47,57 @@ def languages_list(request):
                     data={
                         "message": f"The new language '{data}' is added.",
                         "language": data,
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return Response(data={"message": f"The language already exist"})
+
+        else:
+            return Response(
+                {"error": "Please provide a language name."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+# 2. Class based view
+class languages_class_list(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        languages_list = ProgrammingLanguages.objects.all()
+
+        if languages_list:
+            serializer = ProgrammingLanguages_Serializer(languages_list, many=True)
+            return Response(
+                data={
+                    "message": "Here is the list of Languages",
+                    "languages": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            data={
+                "message": "The list is empty",
+                "languages": None,
+            },
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    def post(self, request):
+        language = request.data.get("language")
+
+        if language:
+            add_language, created = ProgrammingLanguages.objects.get_or_create(
+                language_name=language
+            )
+
+            if created:
+                return Response(
+                    data={
+                        "message": f"The new language '{language}' is added.",
+                        "language": language,
                     },
                     status=status.HTTP_201_CREATED,
                 )
