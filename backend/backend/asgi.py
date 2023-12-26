062e716be8e88c23d027d2d1afad5c5a86332d46
Gaurav Joshi <gj451026@gmail.com>
2023-12-26 11:20:41 +0530

    project structure updated

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -4,19 +4,13 @@ ASGI config for backend project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": application
-})
