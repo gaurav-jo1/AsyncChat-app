6f4a6be0725a0904c129d318ae6e10b4d5e3c86c
gaurav <gj451026@gmail.com>
2024-05-15 09:39:08 +0530

    Error Handling in Auth

--- backend/chats/middleware.py
+++ backend/chats/middleware.py
@@ -1,10 +1,12 @@
-from channels.middleware import BaseMiddleware
 from rest_framework_simplejwt.tokens import AccessToken
 from channels.db import database_sync_to_async
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AnonymousUser
-import jwt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.exceptions import TokenBackendError
 
 
 class JWTAuthMiddleware:
@@ -22,33 +24,48 @@ class JWTAuthMiddleware:
                     "reason": "Invalid token parameter",
                 }
             )
-            return
 
         token = query_params.split("=")[1]
+
         scope["token"] = token
-        scope["user"] = await get_user(scope)
+        try:
+            user = await get_user(scope)
+        except TokenBackendError as ex:
+            print(f"Token Backend Error: {ex}")
+            await send(
+                {
+                    "type": "websocket.close",
+                    "code": 4011,  # Custom code for authentication failure
+                    "reason": "Token is invalid or expired",
+                }
+            )
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            await send(
+                {
+                    "type": "websocket.close",
+                    "code": 5000,  # Custom code for internal server error
+                    "reason": "Internal server error",
+                }
+            )
+            return
+
+        if isinstance(user, AnonymousUser):
+            return user
+
+        scope["user"] = user
 
         return await self.app(scope, receive, send)
 
 
 @database_sync_to_async
 def get_user(scope):
-    if "token" not in scope:
-        raise ValueError(
-            "Cannot find token in scope. You should wrap your consumer in "
-            "TokenAuthMiddleware."
-        )
     token = scope["token"]
-    user = None
-    try:
-        access_token = AccessToken(token)
-        user_id = access_token.payload.get("user_id")
-        user = User.objects.get(id=user_id)
-    except jwt.ExpiredSignatureError:
-        raise AuthenticationFailed("Token has expired")
-    except jwt.InvalidTokenError:
-        raise AuthenticationFailed("Invalid token")
-    except User.DoesNotExist:
-        raise AuthenticationFailed("User not found")
+
+    access_token = AccessToken(token)
+
+    user_id = access_token.payload.get("user_id")
+
+    user = User.objects.get(id=user_id)
 
     return user or AnonymousUser()
