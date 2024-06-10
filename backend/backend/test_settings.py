4a8e74eec10394eb9b9622edc5530a75d536b758
Gaurav Joshi <gj451026@gmail.com>
2024-06-10 12:27:49 +0530

    redis config updated

--- backend/backend/test_settings.py
+++ backend/backend/test_settings.py
@@ -135,8 +135,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis",6379,)
-            ],
+            "hosts": [("redis",6379)],
         },
     },
 }
